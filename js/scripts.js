e111cfba8f012129b35f72bf69187f18adf09f37
everettanthony <mrtonybradshaw@gmail.com>
2022-04-09 14:47:22 -0500

    Removed mobile count box

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@
 	const stateLinks = document.querySelectorAll('#states a.hint');
 	const countDisplay = document.querySelector('.state-count');
 	const countVal = document.querySelector('.state-count-val');
-	const mobileCountVal = document.querySelector('.mobile-count');
 	const spinner = document.querySelector('.spinner-grow');
 	const btnReset = document.querySelector('.btn-reset');
 	const btnShare = document.querySelector('.btn-share');
@@ -47,7 +46,7 @@
     const shareData = {
       files: filesArray,
       title: document.title,
-			text: `I've visited ${mobileCountVal} states in the U.S.`,
+			text: `I've visited ${countVal} states in the U.S.`,
     };
 
     if (navigator.canShare && navigator.canShare({ files: filesArray })) {
@@ -90,16 +89,13 @@
 	function countUpdate(arr) {	
 		if (arr.length > 0) {
 			countVal.textContent = arr.length;
-			mobileCountVal.textContent = arr.length;
 			btnReset.classList.remove('hidden');
 		  btnShare.classList.remove('hidden');
-			mobileCountVal.classList.remove('hidden');
 		}
 		else {
 			countDisplay.classList.add('hidden');		
 			btnReset.classList.add('hidden');
 		  btnShare.classList.add('hidden');
-			mobileCountVal.classList.add('hidden');
 		}
 	}
 })();
